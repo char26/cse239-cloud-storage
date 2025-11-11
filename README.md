ea7dcfeaea8b0925034da50dd950298a8ccfd6a8
char26 <charliealders26@gmail.com>
2025-11-11 06:38:58 -0800

    Initial Terraform infrastructure

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # cse239-cloud-storage
+
+## Installation Prerequisites
+
+### Install Terraform
+
+#### macOS
+
+```sh
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
+```
+
+#### Ubuntu/Debian
+
+```sh
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+sudo apt-get install terraform
+```
+
+### Install gcloud CLI
+
+Follow the prompts from the interactive installer (Linux and macOS)
+
+```sh
+curl https://sdk.cloud.google.com | bash
+
+exec -l $SHELL
+
+gcloud init
+```
+
+## Deployment
+
+These instructions assume you already have a GCP Project created with the Google Compute Engine API enabled. If you do not have this, follow [the instructions from Terraform](https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#set-up-gcp).
+
+Authenticate
+
+```sh
+gcloud auth application-default login
+```
+
+Create the infrastructure
+
+```sh
+terraform init
+terraform fmt
+terraform validate
+terraform apply
+```
+
+Destroy the infrastructure
+
+```sh
+terraform destroy
+```
---
+++ databases-tf/main.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "cloud-storage-477719"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_instance" "postgres_vm" {
+  name         = "postgres-vm"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  metadata_startup_script = <<EOF
+    #!/bin/bash
+    docker run -e POSTGRES_PASSWORD=changeme -d --name postgres -p 5432:5432 postgres:18
+  EOF
+
+  tags = ["postgres-vm"]
+}
+
+resource "google_compute_instance" "scylla_vm" {
+  name         = "scylla-vm"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  metadata_startup_script = <<EOF
+    #!/bin/bash
+    docker run --name scylla --hostname scylla -d scylladb/scylla
+  EOF
+
+  tags = ["scylla-vm"]
+}
+
+
+resource "google_compute_firewall" "postgres_firewall" {
+  name    = "postgres-firewall"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["5432"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["postgres-vm"]
+}
+
+resource "google_compute_firewall" "scylla_firewall" {
+  name    = "scylla-firewall"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["9042"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["scylla-vm"]
+}
