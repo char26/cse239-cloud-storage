a081f0ea7c795b6ca71bd5b5cb9b197c2221a345
Aidan <aitgray@ucsc.edu>
2025-11-18 13:52:25 -0800

    Made changes to make the bench file work via a remote VM instead of locally.

--- bench.sh
+++ bench.sh
@@ -1,8 +1,8 @@
-# Config stuff
-WORKLOAD="" # need to look into YCSB more
+#!/bin/bash
 
-# DB order
-ORDER=("postgres" "scylla")
+# Config stuff
+WORKLOAD="workloads/workloada" # Example YCSB workload file
+NUM_TRIALS=3
 
 # Logs
 OUTDIR="logs"
@@ -13,41 +13,32 @@ PG_USER="user"
 PG_PASSWORD="password"
 PG_DB="database"
 
-# Compose project name
-COMPOSE_PROJECT="bench"
-
-export COMPOSE_PROJECT_NAME="$COMPOSE_PROJECT"
 mkdir -p "$OUTDIR"
 
-# We'll need functions to clean things up, get the host port, wait for the host port to accept connections, etc
-
-# We also need to bring up and stop the services
-bring_up_service() {
-    local service="$1"
-    echo "Bringing up service: $service"
-    docker-compose up -d "$service"
-}
-
-stop_service() {
-    local service="$1"
-    echo "Stopping service: $service"
-    docker-compose down
+# Function to wait for a port to be open
+wait_for_port() {
+    local host="$1"
+    local port="$2"
+    echo "Waiting for $host:$port to be open..."
+    while ! nc -z "$host" "$port"; do
+        sleep 1
+    done
+    echo "$host:$port is open."
 }
 
 # A basic postgres trial
 run_postgres_trial() {
     local trial="$1"
+    local ip="$2"
 
-    bring_up_service "postgres"
+    wait_for_port "$ip" 5432
 
-    # get the postgres port and wait for it to accept the connection
-
-    local DSN="postgresql://${PG_USER}:${PG_PASSWORD}@${IP}:${PORT}/$PG_DB"
+    local DSN="postgresql://${PG_USER}:${PG_PASSWORD}@${ip}:5432/${PG_DB}"
 
     # Make the log file for the trial
     local TSTAMP
     TSTAMP=$(date +"%Y%m%d-%H%M%S")
-    local LOGFILE="${OUTDIR}/postgres/t${trial}-${TSTAMP}"
+    local LOGFILE="${OUTDIR}/postgres/t${trial}-${TSTAMP}.log"
     mkdir -p "$(dirname "$LOGFILE")"
 
     echo "Running Postgres trial $trial, logging to $LOGFILE"
@@ -58,32 +49,46 @@ run_postgres_trial() {
         --dsn "$DSN" \
         --workload "$WORKLOAD" \
         &> "$LOGFILE"
-
-    stop_service "postgres"
 }
 
 # A basic scylla trial
 run_scylla_trial() {
     local trial="$1"
+    local ip="$2"
 
-    bring_up_service "scylla"
-
-    # get the scylla port and wait for it to accept the connection
-
-    # I don't think a DSN is required
+    wait_for_port "$ip" 9042
 
     local TSTAMP
     TSTAMP=$(date +"%Y%m%d-%H%M%S")
-    local LOGFILE="${OUTDIR}/scylla/t${trial}-${TSTAMP}"
+    local LOGFILE="${OUTDIR}/scylla/t${trial}-${TSTAMP}.log"
     mkdir -p "$(dirname "$LOGFILE")"
 
     echo "Running Scylla trial $trial, logging to $LOGFILE"
     $PYTHON benchmark.py \
         --db scylla \
+        --host "$ip" \
         --workload "$WORKLOAD" \
         &> "$LOGFILE"
-
-    stop_service "scylla"
 }
 
-# Finally the logic to actually run the trials
+# --- Main Logic ---
+
+# Get IP addresses from terraform output
+echo "Getting IP addresses from Terraform..."
+POSTGRES_IP=$(terraform -chdir=databases-tf output -raw postgres_public_ip)
+SCYLLA_IP=$(terraform -chdir=databases-tf output -raw scylla_public_ip)
+
+echo "Postgres IP: $POSTGRES_IP"
+echo "Scylla IP: $SCYLLA_IP"
+
+# Run Postgres trials
+for i in $(seq 1 $NUM_TRIALS); do
+    run_postgres_trial "$i" "$POSTGRES_IP"
+done
+
+# Run Scylla trials
+for i in $(seq 1 $NUM_TRIALS); do
+    run_scylla_trial "$i" "$SCYLLA_IP"
+done
+
+echo "Benchmarking complete. Logs are in the '$OUTDIR' directory."
